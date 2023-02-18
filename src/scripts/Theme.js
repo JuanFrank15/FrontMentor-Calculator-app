bda14f226d26ea46b95cb87ce3c5e27c5235c5bd
Juan F Martinez <juanfrank.m15@gmail.com>
2023-02-17 19:00:36 -0600

    feat ( change themes and calculator operation )

---
+++ src/scripts/Theme.js
@@ -0,0 +1,85 @@
+const btn_toggle = document.querySelector(".toggle");
+const btn_result = document.querySelector(".btn_default-result");
+const btn_default = document.getElementsByClassName("btn_default");
+const btn_Del = document.getElementsByClassName("btnDel");
+const bg_keypad = document.getElementsByClassName("bg_keypad");
+const output = document.querySelector(".output_container");
+const body_theme = document.querySelector(".body");
+
+
+
+export function showTheme1() {
+    body_theme.classList.remove("body_theme3");
+    body_theme.classList.add("body_theme1");
+    output.classList.remove("output_theme3");
+    output.classList.add("output_theme1");
+    btn_result.classList.remove("btn_result-theme3");
+    btn_result.classList.add("btn_result-theme1");
+
+    for (let index = 0; index < bg_keypad.length; index++) {
+        bg_keypad[index].classList.remove("bg_toggle_keypad-theme3");
+        bg_keypad[index].classList.add("bg_toggle_keypad-theme1");
+    }
+
+    for (let index = 0; index < btn_default.length; index++) {
+        btn_default[index].classList.remove("btn_default-theme3");
+        btn_default[index].classList.add("btn_default-theme1");
+    }
+
+    for (let index = 0; index < btn_Del.length; index++) {
+        btn_Del[index].classList.remove("btn_del-theme3");
+        btn_Del[index].classList.add("btn_del-theme1");
+    }
+
+}
+
+export function showTheme2() {
+
+    body_theme.classList.remove("body_theme1");
+    body_theme.classList.add("body_theme2");
+    output.classList.remove("output_theme1");
+    output.classList.add("output_theme2");
+    btn_result.classList.remove("btn_result-theme1");
+    btn_result.classList.add("btn_result-theme2");
+
+    for (let index = 0; index < bg_keypad.length; index++) {
+        bg_keypad[index].classList.remove("bg_toggle_keypad-theme1");
+        bg_keypad[index].classList.add("bg_toggle_keypad-theme2");
+    }
+
+    for (let index = 0; index < btn_default.length; index++) {
+        btn_default[index].classList.remove("btn_default-theme1");
+        btn_default[index].classList.add("btn_default-theme2");
+    }
+
+    for (let index = 0; index < btn_Del.length; index++) {
+        btn_Del[index].classList.remove("btn_del-theme1");
+        btn_Del[index].classList.add("btn_del-theme2");
+    }
+
+}
+
+export function showTheme3() {
+    body_theme.classList.remove("body_theme2");
+    body_theme.classList.add("body_theme3");
+    output.classList.remove("output_theme2");
+    output.classList.add("output_theme3");
+    btn_result.classList.remove("btn_result-theme2");
+    btn_result.classList.add("btn_result-theme3");
+
+    for (let index = 0; index < bg_keypad.length; index++) {
+        bg_keypad[index].classList.remove("bg_toggle_keypad-theme2");
+        bg_keypad[index].classList.add("bg_toggle_keypad-theme3");
+    }
+
+    for (let index = 0; index < btn_default.length; index++) {
+        btn_default[index].classList.remove("btn_default-theme2");
+        btn_default[index].classList.add("btn_default-theme3");
+    }
+
+    for (let index = 0; index < btn_Del.length; index++) {
+        btn_Del[index].classList.remove("btn_del-theme2");
+        btn_Del[index].classList.add("btn_del-theme3");
+    }
+}
+
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,109 +1,156 @@
-const btn_toggle = document.querySelector(".toggle");
-const btn_result = document.querySelector(".btn_default-result");
-const btn_default = document.getElementsByClassName("btn_default");
-const btn_Del = document.getElementsByClassName("btnDel");
-const bg_keypad = document.getElementsByClassName("bg_keypad");
-const output = document.querySelector(".output_container");
-const body_theme = document.querySelector(".body");
-console.log(bg_keypad);
-let theme = 0;
+import { showTheme1, showTheme2, showTheme3 } from "./Theme.js"
 
+
+const btn_toggle = document.querySelector(".toggle");
+let theme = 0;
 
 btn_toggle.addEventListener("click", () => {
     ++theme
+
     if (theme == 1) {
-
-        body_theme.classList.remove("body_theme1");
-        body_theme.classList.add("body_theme2");
-
         btn_toggle.classList.remove("theme-1");
         btn_toggle.classList.add("theme-2");
-
-        output.classList.remove("output_theme1");
-        output.classList.add("output_theme2");
-
-        for (let index = 0; index < bg_keypad.length; index++) {
-            bg_keypad[index].classList.remove("bg_toggle_keypad-theme1");
-            bg_keypad[index].classList.add("bg_toggle_keypad-theme2");
-        }
-
-        for (let index = 0; index < btn_default.length; index++) {
-            btn_default[index].classList.remove("btn_default-theme1");
-            btn_default[index].classList.add("btn_default-theme2");
-        }
-
-        for (let index = 0; index < btn_Del.length; index++) {
-            btn_Del[index].classList.remove("btn_del-theme1");
-            btn_Del[index].classList.add("btn_del-theme2");
-        }
-
-        btn_result.classList.remove("btn_result-theme1");
-        btn_result.classList.add("btn_result-theme2");
+        showTheme2();
 
     } else if (theme == 2) {
-        body_theme.classList.remove("body_theme2");
-        body_theme.classList.add("body_theme3");
-
         btn_toggle.classList.remove("theme-2");
         btn_toggle.classList.add("theme-3");
-
-        output.classList.remove("output_theme2");
-        output.classList.add("output_theme3");
-
-        for (let index = 0; index < bg_keypad.length; index++) {
-            bg_keypad[index].classList.remove("bg_toggle_keypad-theme2");
-            bg_keypad[index].classList.add("bg_toggle_keypad-theme3");
-        }
-
-        for (let index = 0; index < btn_default.length; index++) {
-            btn_default[index].classList.remove("btn_default-theme2");
-            btn_default[index].classList.add("btn_default-theme3");
-        }
-
-        for (let index = 0; index < btn_Del.length; index++) {
-            btn_Del[index].classList.remove("btn_del-theme2");
-            btn_Del[index].classList.add("btn_del-theme3");
-        }
-
-        btn_result.classList.remove("btn_result-theme2");
-        btn_result.classList.add("btn_result-theme3");
+        showTheme3();
 
     } else {
-        body_theme.classList.remove("body_theme3");
-        body_theme.classList.add("body_theme1");
-
         btn_toggle.classList.remove("theme-3");
         btn_toggle.classList.add("theme-1");
-
-        output.classList.remove("output_theme3");
-        output.classList.add("output_theme1");
-
-
-        for (let index = 0; index < bg_keypad.length; index++) {
-            bg_keypad[index].classList.remove("bg_toggle_keypad-theme3");
-            bg_keypad[index].classList.add("bg_toggle_keypad-theme1");
-        }
-
-        for (let index = 0; index < btn_default.length; index++) {
-            btn_default[index].classList.remove("btn_default-theme3");
-            btn_default[index].classList.add("btn_default-theme1");
-        }
-
-        for (let index = 0; index < btn_Del.length; index++) {
-            btn_Del[index].classList.remove("btn_del-theme3");
-            btn_Del[index].classList.add("btn_del-theme1");
-        }
-
-        btn_result.classList.remove("btn_result-theme3");
-        btn_result.classList.add("btn_result-theme1");
-
+        showTheme1();
         theme = 0;
-        console.log(theme);
     }
 });
 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
 
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
 
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
 
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
 
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.currentOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
 
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case 'x':
+                computation = prev * current
+                break
+            case '/':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const resetButton = document.querySelector('[data-reset]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+
+    })
+    
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+
+})
+
+resetButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+    
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
